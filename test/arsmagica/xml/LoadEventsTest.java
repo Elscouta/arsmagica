f975857d54f4f168d41e88dc4e8a16e19ce66d42
Rémi Bonnet <git@elscouta.net>
2017-02-09 13:29:24 +0100

    * Test cases for event probabilities

--- test/arsmagica/xml/LoadEventsTest.java
+++ test/arsmagica/xml/LoadEventsTest.java
@@ -5,8 +5,11 @@
  */
 package arsmagica.xml;
 
+import arsmagica.Settings;
 import arsmagica.desc.EntityDesc;
 import arsmagica.desc.EventDesc;
+import arsmagica.model.Entity;
+import arsmagica.model.World;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -22,34 +25,22 @@ import org.junit.Test;
 public class LoadEventsTest 
 {
     private XMLFileLoader<EventDesc> loader;
-    
-    public LoadEventsTest() 
-    {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() 
-    {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() 
-    {
-    }
+    private World world;
+    private Entity testEntity;
     
     @Before
-    public void setUp() 
+    public void setUp() throws Exception
     {
         DataStore store = new DataStore("test/arsmagica/xml/testdata/");
         loader = new XMLFileLoader<>(store, "event", 
                 new EventDesc.Loader(store));
+        world = new World(store);
+        
+        store.loadEntityDescFile("entity_propint_const.xml");
+        testEntity = world.createEntity("test_entity");
+        assertEquals(10, testEntity.get("test_property").asInt().getValue());
     }
     
-    @After
-    public void tearDown() 
-    {
-    }
-
     @Test
     public void loadEventEmpty() throws XMLError
     {
@@ -61,5 +52,25 @@ public class LoadEventsTest
     public void loadEventNoType() throws XMLError
     {
         loader.loadXML("event_notext.xml");        
+    }
+    
+    @Test (expected=XMLError.class)
+    public void loadEventNoProb() throws XMLError
+    {
+        loader.loadXML("event_noprob.xml");
+    }
+    
+    @Test
+    public void loadEventProbConst() throws Exception
+    {
+        EventDesc e = loader.loadXML("event_empty.xml").get(0);
+        assertEquals(1, e.getProbability(world, testEntity), Settings.FLOAT_PRECISION);
+    }
+    
+    @Test
+    public void loadEventProbSource() throws Exception
+    {
+        EventDesc e = loader.loadXML("event_probsource.xml").get(0);
+        assertEquals(0.5, e.getProbability(world, testEntity), Settings.FLOAT_PRECISION);        
     }
 }
